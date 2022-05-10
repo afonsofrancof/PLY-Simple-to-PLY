b502f48813bfe56a433fab007fa42b8ce4107f50
tsousa111 <a93248@uminho.pt>
2022-05-10 12:35:40 +0100

    comeco de um lexer

--- lexer.py
+++ lexer.py
@@ -1,3 +1,30 @@
+from lib2to3.pgen2 import literals
 import ply.lex as lex
 
-tokens = []
+states = (
+    ("lex","exclusive"),
+    ("yacc","exclusive")
+)
+
+reserved = {
+    "return": "RETURN",
+    "error": "ERROR"
+}
+
+tokens = ["TEXT","LEX","YACC","VAL"] + list(reserved.values())
+
+literals = ["[","]","(",")",",","="]
+
+ignore = " \t\n\r"
+
+t_RET = r"return"
+
+
+
+t_TEXT = r""
+
+def t_error(t):
+    print("Illegal Character: ", t.value[0])
+
+
+lexer = lex.lex()
